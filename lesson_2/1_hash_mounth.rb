5db66d40195ee96b8150e805cf0e65a0e88b4d63
Vlad2088 <vladislavganieff@yandex.ru>
2023-02-18 02:27:24 +1200

    prints_the_serial_number_of_the_date

---
+++ lesson_2/1_hash_mounth.rb
@@ -0,0 +1,17 @@
+
+mounth = {
+  :jan => 31,
+  :feb => 28,
+  :mar => 31,
+  :apr => 30,
+  :may => 31,
+  :jun => 30,
+  :jul => 31,
+  :aug => 31,
+  :sep => 30,
+  :oct => 31,
+  :nov => 30,
+  :dec => 31,    
+}
+
+mounth.each {|m, d| puts "#{m}" if d == 30}
