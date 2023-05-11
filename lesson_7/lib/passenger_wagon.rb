24baac2872c9815d2a01793dfc40d9576f1bd0a8
Vlad2088 <vladislavganieff@yandex.ru>
2023-05-12 01:05:50 +1200

    creates_a_wagon_type_passenger

---
+++ lesson_7/lib/passenger_wagon.rb
@@ -0,0 +1,21 @@
+class PassengerWagon < Wagon
+
+  @instances = 0
+
+
+  class << self
+    def instance
+      @instances
+    end
+  end
+
+
+  def initialize(capacity = 0, manufacturer_name)
+    @type = Train::PASSENGER_TYPE
+    super(capacity, manufacturer_name)
+  end
+
+  def take_the_place_of
+    @occupied_place = @occupied_place + 1
+  end
+end
